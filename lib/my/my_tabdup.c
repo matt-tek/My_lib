f185b818ea78d17a451e9e788d596f8779070f02
matt@Matt <mathieu.muty@epitech.eu>
2021-05-05 11:29:37 +0400

    add tabdup

---
+++ lib/my/my_tabdup.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2020
+** B-PSU-101-RUN-1-1-minishell1-mathieu.muty
+** File description:
+** my_tabdup.c
+*/
+
+#include "../include/my.h"
+#include "../include/minish.h"
+
+char **my_tabdup(char **array)
+{
+    int i = 0;
+    char **tab = malloc(sizeof(char *) * (my_tablen(array) + 1));
+
+    if (tab == NULL) {
+        free(tab);
+        return (NULL);
+    }
+    for (; array[i] != NULL; i++)
+        tab[i] = my_strdup(array[i]);
+    tab[i] = NULL;
+    return (tab);
+}
